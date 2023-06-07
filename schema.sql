5a57b71935139ca019a03b78659afac9163cc97b
Daniel Falcon <danfall963@gmail.com>
2023-06-06 22:31:51 -0400

    Add vet_clinic Database animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+	id SERIAL PRIMARY KEY,
+	name varchar(20) NOT NULL,
+	date_of_birth date NOT NULL,
+	escape_attempts int NOT NULL DEFAULT 0,
+	neutered boolean NOT NULL DEFAULT false,
+	weight_kg float NOT NULL
 );
